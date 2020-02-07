40ad318947ce5ec6ae8795091063d8d2a341c7ca
sravan7 <sravan7@users.noreply.github.com>
2020-02-07 11:46:13 +0000

    deploy: 47d7a81de3a06e04e468c02562875107accf04b8

---
+++ precache-manifest.38d3d68f7d3d9239e249f8a39cca65a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5b347c7e994bc2b9cf75a25c6e4acd",
+    "url": "/ciSetup/index.html"
+  },
+  {
+    "revision": "99c2820ac1c21e009c44",
+    "url": "/ciSetup/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e12be0f49c230f4bc87b",
+    "url": "/ciSetup/static/js/2.17fe7fa6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ciSetup/static/js/2.17fe7fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c2820ac1c21e009c44",
+    "url": "/ciSetup/static/js/main.d5a93938.chunk.js"
+  },
+  {
+    "revision": "a60041c0092916507a91",
+    "url": "/ciSetup/static/js/runtime-main.4c80139e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ciSetup/static/media/logo.5d5d9eef.svg"
+  }
+]);
