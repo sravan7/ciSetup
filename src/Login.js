40fad47081f16255cd8e2947936558f3870acb75
sravan7 <esd14i017@iiitdm.ac.in>
2020-02-08 08:55:27 +0530

    login component

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+
+
+
+function Login(){
+
+    return (
+        <div className="App-login"> 
+            login 
+        </div>
+    );
+
+};
+
+export default Login;
