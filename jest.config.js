ada3dfdfca8f332ef64345acf90c51a647b09286
sravan7 <esd14i017@iiitdm.ac.in>
2020-02-28 18:26:28 +0530

    removed jest confid=f puppeteer

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
     "verbose": true,
-    "preset": "jest-puppeteer",
-    globals: {
-      URL: "http://localhost:3000"
-    },
     "setupFiles": ["jest-localstorage-mock"]
   };
